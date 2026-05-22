689c200d4717b3018c5ffb05e775162951022a31
Ahtsham Elahi <ahtshamelahi6660@gmail.com>
2026-05-22 11:20:16 -0700

    Update api.js

--- disease-predictor-ai/frontend/js/api.js
+++ disease-predictor-ai/frontend/js/api.js
@@ -1,5 +1,5 @@
 async function sendAnswers(disease, answers) {
-    const response = await fetch(`http://127.0.0.1:5000/api/predict/${disease}`, {
+    const response = await fetch(`https://mediscanai-nvdx.onrender.com/api/predict/${disease}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ answers })
